acda89773c433f09103b4cf3d3d1b2ce052c2978
jpkhawam <jeanpaulkhawam@tutanota.com>
2022-07-16 02:02:30 +0300

    extracted structs & enums to separate file

--- functions.h
+++ functions.h
@@ -8,34 +8,7 @@
 #include <GL/glut.h>
 #include <cmath>
 #include <string>
-
-extern bool g_MODE_SIN;
-extern bool g_MODE_COS;
-extern bool g_MODE_TAN;
-extern bool g_MODE_ASIN;
-extern bool g_MODE_ACOS;
-extern bool g_MODE_ATAN;
-extern float g_offset;
-
-enum equations {
-    sine = 0,
-    cosine = 1,
-    tangent = 2,
-    pause = 3,
-    arcSine = 4,
-    arcCosine = 5,
-    arcTangent = 6
-};
-
-struct RGB {
-    float red, green, blue;
-
-    RGB(float red, float green, float blue) {
-        this->red = red;
-        this->green = green;
-        this->blue = blue;
-    }
-};
+#include "types.h"
 
 bool allPaused() {
     return g_MODE_SIN || g_MODE_COS || g_MODE_TAN || g_MODE_ASIN || g_MODE_ACOS || g_MODE_ATAN;
